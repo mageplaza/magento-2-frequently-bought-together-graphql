2455f3edbd79eea8f0496779362dbdf9f2c5bcd8
bruno <cuongpn@mageplaza.com>
2021-08-09 14:29:54 +0700

    add get config api

---
+++ Model/Resolver/Config.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_FrequentlyBoughtGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+declare(strict_types=1);
+
+namespace Mageplaza\FrequentlyBoughtGraphQl\Model\Resolver;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\FrequentlyBought\Helper\Data;
+
+/**
+ * Class Config
+ * @package Mageplaza\FrequentlyBoughtGraphQl\Model\Resolver
+ */
+class Config implements ResolverInterface
+{
+    /**
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * Config constructor.
+     *
+     * @param Data $helperData
+     */
+    public function __construct(
+        Data $helperData
+    )
+    {
+        $this->helperData = $helperData;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!$this->helperData->isEnabled()) {
+            throw new LocalizedException(__('The module is disabled'));
+        }
+
+        return $this->getConfig();
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [
+            "isEnable" => $this->helperData->isEnabled(),
+            "product_method" => $this->helperData->getConfigGeneral('product_method'),
+            "block_name" => $this->helperData->getConfigGeneral('block_name'),
+            "item_limit" => $this->helperData->getConfigGeneral('item_limit'),
+            "enable_add_to_wishlist" => $this->helperData->getConfigGeneral('enable_add_to_wishlist'),
+            "remove_related_block" => $this->helperData->getConfigGeneral('remove_related_block'),
+            "separator_image" => $this->helperData->getConfigGeneral('separator_image'),
+            "use_popup" => $this->helperData->getConfigGeneral('use_popup')
+        ];
+    }
+}
